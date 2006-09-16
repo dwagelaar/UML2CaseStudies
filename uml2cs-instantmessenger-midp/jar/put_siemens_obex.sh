0bbc9d0ddd725f32d35f527a43a2f79282cf1ee0
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2006-09-16 19:25:47 +0000

    self-sufficient copy

---
+++ uml2cs-instantmessenger-midp/jar/put_siemens_obex.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+obexftp -c Data/Java/jam/Games -C InstantMessenger
+obexftp -c Data/Java/jam/Games/InstantMessenger -p icon*
+obexftp -c Data/Java/jam/Games/InstantMessenger -p instantmessenger.jad
+obexftp -c Data/Java/jam/Games/InstantMessenger -p instantmessenger.jar

0ca1a7f46b0959c06e268b015a78cb4c8d036560
dwagelaa <dwagelaa@5ad673d7-9de5-0310-9841-9683b5787651>
2006-09-27 14:50:16 +0000

    Set up build and deployment environment

--- uml2cs-instantmessenger-midp/jar/put_siemens_obex.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-obexftp -c Data/Java/jam/Games -C InstantMessenger
-obexftp -c Data/Java/jam/Games/InstantMessenger -p icon*
-obexftp -c Data/Java/jam/Games/InstantMessenger -p instantmessenger.jad
-obexftp -c Data/Java/jam/Games/InstantMessenger -p instantmessenger.jar
